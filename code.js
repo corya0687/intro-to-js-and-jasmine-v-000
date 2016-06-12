14633cc4ccdbc48af131444eaeb5c516f6e7e88c
corya0687 <corya0687@gmail.com>
2016-06-12 16:29:18 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(name) {
+  return "I love " + name;
+}
+
+function shouting(phrase) {
+  return phrase.toUpperCase();
+}
+
+function roundDown(someNumber){
+  return Math.floor(someNumber);
+}
+
+function theTruth() {
+  return true;
+}
